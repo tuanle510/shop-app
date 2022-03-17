c3aab589b098a7b461035aef903e027e7103b3f7
Justin Le <lethientuan8668@gmail.com>
2022-03-17 22:01:22 +0700

    cart update

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,11 +2,18 @@ import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer,
-  
+  cart: cartReducer
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
